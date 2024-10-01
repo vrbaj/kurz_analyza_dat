50f1019a49232922d5aacbaa8deeac41130b21ef
vrbaj <vrbik.vrbik@gmail.com>
2024-10-01 08:25:48 +0200

    01_funkce.py přidány

---
+++ utery/01_funkce.py
@@ -0,0 +1,12 @@
+cislo = -5.6123
+print(abs(cislo))  # absolutní hodnota čísla
+print(chr(35))  # vrací unicode znak podle kódu
+print(ord("#"))  # vrací unicode znaku - použitelné pro zjištění netisknutelných znaků
+
+seznam = [1, 2, 3, 6.5]
+print(max(seznam))  # maximální hodnota z iterovatelného objektu
+print(min(seznam))  # minimální hodnota z iterovatelného objektu
+
+print(round(cislo, 2))  # zaokrouhlení čísla na 2 des. místa
+
+print(sum(seznam))  # vrací součet prvků it. objektu
