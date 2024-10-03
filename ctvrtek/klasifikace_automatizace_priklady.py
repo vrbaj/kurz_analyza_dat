d4bcdee02131e9924bf7fa85a823072a7dbabc53
vrbaj <vrbik.vrbik@gmail.com>
2024-10-03 14:40:19 +0200

    přidán skript na klasifikaci

--- ctvrtek/klasifikace_automatizace_priklady.py
+++ ctvrtek/klasifikace_automatizace_priklady.py
@@ -30,7 +30,9 @@ X_train, X_test, y_train, y_test = train_test_split(X, y,
                                                     test_size=0.4, random_state=42)
 # Až sem stačí provést jednou!
 # Seznam objektů pro jednotlivé klasifikátory
-seznam_clf = [SVC(), DecisionTreeClassifier(), RandomForestClassifier()]
+seznam_clf = [("svm", SVC()), # SVM model
+              ("tree", DecisionTreeClassifier()), # Rozhodovací strom
+              ("rf", RandomForestClassifier())] # Náhodný les
 seznam_nastaveni_parametru = [
     # Nastavení hyperparametrů pro SVM
     {"svm__C": [0.1, 1, 10], # Regularizační parametr
@@ -49,43 +51,45 @@ seznam_nastaveni_parametru = [
      "rf__criterion": ["gini", "entropy"], # Kriteriální funkce pro rozdělení vzorků v uzlu
      "rf__bootstrap": [True, False]}, # Volba, zdali má být použit bootstrapping
 ]
-# Vytvoření pipeline pro SVM klasifikaci
-pipeline = Pipeline([
-    ('scaler', StandardScaler()), # Škálování dat různých rozsahů na stejný interval
-    ("svm", SVC()) # SVM klasifikátor
-])
+# Iterace skrz jednotlivé klasifikátory a jejich nastavení parametrů
+for klasifikator, nastaveni_parametru in zip(seznam_clf, seznam_nastaveni_parametru):
+    # Vytvoření pipeline pro zvolený klasifikátor
+    pipeline = Pipeline([
+        ('scaler', StandardScaler()), # Škálování dat různých rozsahů na stejný interval
+        klasifikator # Zvolený klasifikátor
+    ])
 
-# Rozsah hyperparametrů
-rozsah_parametru = {
-    "svm__C": [0.1, 1, 10], # Parametr 'C' pro objekt 'svm' v pipeline
-    "svm__kernel": ["linear", "rbf"], # Parametr 'kernel' pro objekt 'svm' v pipeline
-    "svm__gamma": ["scale", "auto"] # Parametr 'gamma' pro objekt 'svm' v pipeline
-}
+# # Rozsah hyperparametrů
+# rozsah_parametru = {
+#     "svm__C": [0.1, 1, 10], # Parametr 'C' pro objekt 'svm' v pipeline
+#     "svm__kernel": ["linear", "rbf"], # Parametr 'kernel' pro objekt 'svm' v pipeline
+#     "svm__gamma": ["scale", "auto"] # Parametr 'gamma' pro objekt 'svm' v pipeline
+# }
 
-# Grid search (zkoušení všech kombinací hyperparametrů
-grid_search = GridSearchCV(pipeline, rozsah_parametru, cv=5, scoring='accuracy')
-# Trénování dat pro všechny kombinace hyperparametrů
-grid_search.fit(X_train, y_train)
+    # Grid search (zkoušení všech kombinací hyperparametrů
+    grid_search = GridSearchCV(pipeline, nastaveni_parametru, cv=5, scoring='accuracy')
+    # Trénování dat pro všechny kombinace hyperparametrů
+    grid_search.fit(X_train, y_train)
 
-# Vypsání nejlepších hyperparametrů pro SVM
-print("-------------SVM-------------")
-# grid_search.best_params_ vrací nastavení pro model s nejvyšší dosaženou přesností
-# podle křížové validace
-print(f"Nejlepší nastavení SVM: {grid_search.best_params_}.")
-# Zjištění skóre z křížové validace trénovacích dat
-cv_skore = cross_val_score(grid_search.best_estimator_, X_train, y_train, cv=5, scoring="accuracy")
-cv_skore_text = []
-for skore in cv_skore:
-    cv_skore_text.append(f"{skore:.2%}")
-print(f"Skóre z iterací křížové validace: {', '.join(cv_skore_text)}")
-# Testování modelu na neznámých datech
-test_score = grid_search.score(X_test, y_test)
-print(f"Testovací přesnost: {test_score:.2%}")
-# Matice záměn
-y_predikce = grid_search.predict(X_test)
-matice_zamen = confusion_matrix(y_test, y_predikce)
-TN, FP, FN, TP = matice_zamen.ravel()
-print(f"TP: {TP}, TN: {TN}, FP: {FP}, FN: {FN}")
-print(f"Senzitivita: {recall_score(y_test, y_predikce):.2%}, "
-      f"Specificita: {recall_score(y_test, y_predikce, pos_label=0):.2%}, "
-      f"PPV: {precision_score(y_test, y_predikce):.2%}")
+    # Vypsání nejlepších hyperparametrů pro zvolený klasifikátor
+    print(f"-------------{klasifikator[0]}-------------")
+    # grid_search.best_params_ vrací nastavení pro model s nejvyšší dosaženou přesností
+    # podle křížové validace
+    print(f"Nejlepší nastavení SVM: {grid_search.best_params_}.")
+    # Zjištění skóre z křížové validace trénovacích dat
+    cv_skore = cross_val_score(grid_search.best_estimator_, X_train, y_train, cv=5, scoring="accuracy")
+    cv_skore_text = []
+    for skore in cv_skore:
+        cv_skore_text.append(f"{skore:.2%}")
+    print(f"Skóre z iterací křížové validace: {', '.join(cv_skore_text)}")
+    # Testování modelu na neznámých datech
+    test_score = grid_search.score(X_test, y_test)
+    print(f"Testovací přesnost: {test_score:.2%}")
+    # Matice záměn
+    y_predikce = grid_search.predict(X_test)
+    matice_zamen = confusion_matrix(y_test, y_predikce)
+    TN, FP, FN, TP = matice_zamen.ravel()
+    print(f"TP: {TP}, TN: {TN}, FP: {FP}, FN: {FN}")
+    print(f"Senzitivita: {recall_score(y_test, y_predikce):.2%}, "
+          f"Specificita: {recall_score(y_test, y_predikce, pos_label=0):.2%}, "
+          f"PPV: {precision_score(y_test, y_predikce):.2%}")
