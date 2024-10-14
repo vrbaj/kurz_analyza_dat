6b0e0df6a7ad0479202a011189634e2d30365737
vrbaj <vrbik.vrbik@gmail.com>
2024-10-14 15:14:24 +0200

    Pridan skript na ovladani pandas podobne jako sql.

--- tyden2/pondeli/pandas_sql.py
+++ tyden2/pondeli/pandas_sql.py
@@ -89,4 +89,78 @@ print(df_transakce.isna().sum())
 print("\n-------VRACENI VSECH TRANSAKCI PRO ZVOLENA ICO-------")
 print(df_transakce[df_transakce["ico"].isin(["76767", "05902133"])])
 print("\n-------VRACENI VSECH TRANSAKCI PRO CAST ICO-------")
-print(df_transakce[df_transakce["ico"].str.contains("67") | df_transakce["ico"].str.contains("133")])
+print(df_transakce[df_transakce["ico"].str.contains("67") |
+                   df_transakce["ico"].str.contains("133")])
+
+# Ukazka shlukovani pomoci GROUP BY
+print("\n-------POCTY TRANSAKCI PODLE JEDNOTLIVYCH MEN-------")
+# SELECT mena, COUNT(mena)
+# FROM transakce
+# GROUP BY mena
+print(df_transakce.groupby("mena").size())
+
+print("\n-------CELKOVA SUMA A PRUMERNA VYSE TRANSAKCI PODLE MEN-------")
+# SELECT mena, SUM(castka), AVG(castka)
+# FROM transakce
+# GROUP BY mena
+print(df_transakce.groupby("mena").agg({"castka": ["sum", "mean"]}))
+
+# Shlukovani pomoci vice sloupcu
+print("\n-------SUMA TRANSAKCI PODLE ICO A MEN-------")
+# SELECT ico, mena, SUM(castka)
+# FROM transakce
+# GROUP BY ico, mena
+print(df_transakce.groupby(["ico", "mena"])["castka"].sum())
+
+# Slucovani tabulek podle klice
+print("\n-------SLOUCENI OBOU TABULEK PODLE ICO-------")
+# SELECT * FROM partneri
+# LEFT JOIN transakce
+# ON partneri.ico = transakce.ico
+print(df_partneri.merge(df_transakce, left_on="ico", right_on="ico", how="left"))
+# print(df_partneri.merge(df_partneri, on="ico", how="left").columns)
+
+# Slucovani pomoci indexu
+transakce_alt = df_transakce.set_index("ico")
+print("\n-------SLOUCENI TABULKY PRES INDEX-------")
+print(df_partneri.merge(transakce_alt, left_on="ico", right_index=True, how="left"))
+
+# Slouceni pomoci UNION
+# SELECT * FROM partneri
+# UNION
+# SELECT * FROM partneri
+print("\n-------SLOUCENI POMOCI UNION-------")
+print(pd.concat([df_partneri, df_partneri]))# odstraneni duplicit .drop_duplicates(keep="first")
+
+# Razeni radku tabulky
+print("\n-------RAZENI RADKU TABULKY-------")
+# SELECT * FROM transakce
+# ORDER BY datum DESC
+print(df_transakce.sort_values("datum", ascending=False))
+
+# Modifikace hodnot v tabulce
+print("\n-------ZMENA MENY NA HUF PRO TRANSAKCE U ICO 29776767-------")
+# UPDATE transakce
+# SET mena = 'HUF'
+# WHERE ico = '29776767'
+
+df_transakce.loc[df_transakce["ico"] == "29776767", "mena"] = "HUF"
+print(df_transakce.loc[df_transakce["ico"] == "29776767", "mena"])
+
+# Mazani casti tabulky
+print("\n-------SMAZANI TRANSAKCI PRO ICO '74521056'-------")
+# DELETE FROM transakce
+# WHERE ico = '74521056'
+df_transkace = df_transakce[df_transakce["ico"] != "74521056"]
+
+# Smazani celeho sloupce castka_tis primo v tabulce
+df_transakce.drop("castka_tis", axis=1, inplace=True)
+print(df_transakce.columns)
+
+# Smazani vsech radku s NULL hodnotami primo v tabulce
+print(df_transakce.shape)
+df_transakce.dropna(axis=0, inplace=True)
+print(df_transakce.shape)
+
+#TODO: Vsechny korunove transakce v cervnu 2023
+#TODO: Soucty EUR transakci pro jednotlive firmy (udaj firmy bude jeji nazev misto ICO)
