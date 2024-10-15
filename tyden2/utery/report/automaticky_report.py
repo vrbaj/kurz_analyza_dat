912c4c346a62adb0fcf139cce0090ab1c3312d0e
vrbaj <vrbik.vrbik@gmail.com>
2024-10-15 15:13:10 +0200

    přidán soubor pro vytvoření reportu

--- tyden2/utery/report/automaticky_report.py
+++ tyden2/utery/report/automaticky_report.py
@@ -30,3 +30,52 @@ datum_do_pro_tisk = ".".join(datum_do_seznam)
 # Pocet vybranych pokut a celkova vybrana castka
 pocet_pokut = data_kontroly[data_kontroly["penalty"] > 0].shape[0]
 celkova_vybrana_castka = data_kontroly["penalty"].sum()
+
+# Vytvoreni objektu pro dokument
+dokument = SimpleDocTemplate("report.pdf", pagesize=A4)
+max_sirka = dokument.width
+max_vyska = dokument.height
+print(max_sirka, max_vyska)
+# Nacteni preddefinovanych stylu pro text a dokument
+styly = getSampleStyleSheet()
+# Vlastni nastaveni stylu pro text a tabulku
+styl_nazev = ParagraphStyle(
+    name="nazev", # nazev stylu
+    fontSize=48, # velikost fontu
+    alignment=1, # zarovnani na stred
+    spaceBefore=0, # mezera pred odstavcem
+    spaceAfter=0, # mezera za odstavcem
+    leading=48, # radkovani, minimalne stejne jako velikost fontu
+    fontName="Courrier" # nazev fontu, vyuzivame vlastni definovany font
+)
+
+styl_nadpis1 = ParagraphStyle(
+    name="nadpis1",
+    fontSize=24,
+    spaceBefore=12,
+    spaceAfter=8,
+    leading=24,
+    fontName="Courrier"
+)
+
+styl_nadpis2 = ParagraphStyle(
+    name="nadpis2",
+    fontSize=18,
+    spaceBefore=12,
+    spaceAfter=8,
+    leading=18,
+    fontName="Courrier"
+)
+# Styl pro normalni text z preddefinovaneho stylu
+styl_normalni = styly["Normal"]
+styl_normalni.name = "normalni"
+styl_normalni.fontName = "Arial"
+
+# Prvni stranka dokumentu - nazev
+nazev_dokumentu = Paragraph("Automatizovaný report", styl_nazev)
+
+# Ulozeni jednotlivych casti dokumentu
+prvky_dokumentu = [nazev_dokumentu]
+
+# Vygenerovani dokumentu
+dokument.build(prvky_dokumentu)
