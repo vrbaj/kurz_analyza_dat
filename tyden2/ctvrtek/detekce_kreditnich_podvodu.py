bde5c583f776aa60e426d879df151f9a440d64c2
vrbaj <vrbik.vrbik@gmail.com>
2024-10-17 08:53:45 +0200

    aktualizace detekce_kreditnich_podvodu.py

--- tyden2/ctvrtek/detekce_kreditnich_podvodu.py
+++ tyden2/ctvrtek/detekce_kreditnich_podvodu.py
@@ -22,3 +22,25 @@ from sklearn.metrics import matthews_corrcoef, make_scorer
 from imblearn.pipeline import Pipeline
 from imblearn.over_sampling import SMOTE
 from imblearn.under_sampling import NearMiss
+
+## Nahrani dat a zakladni analyza
+data = pd.read_csv("creditcard.csv")
+# Rozmery tabulky
+print(f"Dimenze dat: {data.shape}")
+# Prehled sloupcu
+print(f"Sloupce: {(', ').join(data.columns.tolist())}")
+
+# Datove typy
+print("\n-----Datove typy-----")
+print(data.info())
+
+# Informace o datech
+print("\n-----Informace o datech-----")
+pd.set_option("display.max_columns", None) # potlaceni omezeni poctu zobrazenych sloupcu
+print(data.describe().T)
+
+print("\nZ dat vidime, ze jsou vyrazne nevyvazene jednotlive tridy:")
+print(f"-\tpocet vzorku tridy 1 (podezrele transakce): {data['Class'].value_counts().loc[1]}")
+print(f"-\tpocet vzorku tridy 0 (normalni transakce): {data['Class'].value_counts().loc[0]}")
+pomer_trid = data['Class'].value_counts().loc[1] / data['Class'].value_counts().loc[0]
+print(f"-\tpomer mezi tridami 1/0: {pomer_trid:.4%}")
