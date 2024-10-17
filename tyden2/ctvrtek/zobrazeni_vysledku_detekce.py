f99e5a679ecd8dd15acb00a809ab73a4ecefdd07
vrbaj <vrbik.vrbik@gmail.com>
2024-10-17 14:00:15 +0200

    přidání zobrazeni_vysledku_detekce.py

---
+++ tyden2/ctvrtek/zobrazeni_vysledku_detekce.py
@@ -0,0 +1,10 @@
+from matplotlib import pyplot as plt
+import pandas as pd
+import pickle
+import seaborn as sns
+import numpy as np
+
+# Metriky pro hodnoceni kvality regulace
+from sklearn.metrics import accuracy_score, balanced_accuracy_score, recall_score,
+from sklearn.metrics import confusion_matrix, matthews_corrcoef
+
