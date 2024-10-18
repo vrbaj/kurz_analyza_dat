34e89bdb32981555679b5ac932482d8f7e1be067
vrbaj <vrbik.vrbik@gmail.com>
2024-10-18 10:05:14 +0200

    přidání report_subjektu.py

---
+++ tyden2/patek/report_subjektu.py
@@ -0,0 +1,8 @@
+from docx import Document
+from docx.shared import Cm, Pt
+from docx.enum.text import WD_ALIGN_PARAGRAPH, WD_PARAGRAPH_ALIGNMENT
+import pandas as pd
+pd.set_option("display.width", 10000)
+pd.set_option("display.max_columnwidth", None)
+pd.set_option("display.max_columns", None)
+import json
