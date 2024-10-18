7e51bda2a0d9080f282fd0c5d937993dd1973e3d
vrbaj <vrbik.vrbik@gmail.com>
2024-10-18 08:52:03 +0200

    aktualizace zobrazeni_vysledku_detekce.py

--- tyden2/patek/plyny_odchylky.py
+++ tyden2/patek/plyny_odchylky.py
@@ -26,3 +26,21 @@ for sheet in sheety:
     # Odstraneni mezer ze sloupce id_entity
     tabulky[-1]["id_entity"] = tabulky[-1]["id_entity"].str.replace(" ", "")
     print(tabulky[-1].head(), end="\n\n")
+
+## Slouceni tabulek
+data_sloucena = pd.merge(tabulky[0], tabulky[1], on=["id_entity", "vyrobek"], how="outer",
+                         suffixes=("_skut", "_vyk"))
+# Nahrazeni NaN nulami
+data_sloucena.fillna(0, inplace=True)
+data_sloucena["vyrobek_zkraceny"] = data_sloucena["vyrobek"].apply(lambda x: x.split(" - ")[0])
+# Zmena datoveho typu pro roky na int
+seznam_sloupcu = ["2023_skut", "2024_skut", "2023_vyk", "2024_vyk"]
+for sloupec in seznam_sloupcu:
+    data_sloucena[sloupec] = data_sloucena[sloupec].astype(int)
+print(data_sloucena.head(), end="\n\n")
+
+## Vyrobky
+vsechny_vyrobky = data_sloucena["vyrobek"].unique()
+vsechny_vyrobky_zkracene = data_sloucena["vyrobek_zkraceny"].unique()
+# Vypsani seznamu unikatnich vyrobku
+print(f"Vsechny vyrobky:\n-\t{('\n-\t').join(vsechny_vyrobky.tolist())}")
