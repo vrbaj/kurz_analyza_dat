6f0979501068b83c4591d7f108cfaf9fa477a557
stein <steinbach.jakub@gmail.com>
2025-03-17 14:36:27 +0100

    Pondělní lekce - zpracování kontrol OPL

--- tyden3/pondeli/kontroly_opl_ocisteni.py
+++ tyden3/pondeli/kontroly_opl_ocisteni.py
@@ -8,3 +8,10 @@ from tabulate import tabulate
 
 # Načtení dat ke kontrolám OPL
 data = pd.read_excel("OPL_VSCHT.xlsx", decimal=",")
+# Smazání nevyhovujících sloupců
+mazane_sloupce = ["Duplicita", "Kontrolni_Cinnost", "Mesto",
+                  "Km", "Cas_Zjisteni", "CisloTydne",
+                  "Popis_Mista_Zjisteni", "Kontrola_ID",
+                  "Rozkaz_ID", "Typ_Vozidla",
+                  "Bydliste_Porusitele"]
+data_ocistena = data.drop(columns=mazane_sloupce)
