da6e31454890663661098da6ab84ccd6f609aa82
stein <steinbach.jakub@gmail.com>
2025-03-17 13:25:09 +0100

    Pondělní lekce - zpracování kontrol OPL

---
+++ tyden3/pondeli/kontroly_opl.py
@@ -0,0 +1,7 @@
+# Importy knihoven
+import pandas as pd
+import geopandas as gpd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from shapely.geometry import Polygon
+from tabulate import tabulate
