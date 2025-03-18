9b1f337ab892d067adadc96cd627860e6f349cb5
stein <steinbach.jakub@gmail.com>
2025-03-18 10:14:53 +0100

    Pondělní lekce - zpracování kontrol OPL

--- tyden3/pondeli/opl_korelace.py
+++ tyden3/pondeli/opl_korelace.py
@@ -1,3 +1,11 @@
 # Importy
 import pandas as pd
 from scipy.stats import pearsonr
+
+# Načtení dat
+data_cetnosti = pd.read_csv("cetnosti_dle_opl.csv")
+data_index = pd.read_excel("Index-sociálního-vyloučení-2023.xlsx")
+data_index = data_index[["Název kraj",
+                         "Počet obyvyatel - starších 15 let (k 31. 12. 2023)",
+                         "Index soc. vyloučení 2023 (0-30 bodů)"]]
+
