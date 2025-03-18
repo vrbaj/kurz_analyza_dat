bb199a717cdec78aff368b08a198feb15684ce6e
stein <steinbach.jakub@gmail.com>
2025-03-18 12:58:37 +0100

    Úterní lekce - zpracování kontrolních činností

--- tyden3/utery/kontroly_nacteni_cisteni.py
+++ tyden3/utery/kontroly_nacteni_cisteni.py
@@ -1,7 +1,17 @@
 # Importy
 import pandas as pd
+from tabulate import tabulate
 
-# Načtení dat
-data_kontroly = pd.read_excel("Kontroly_VSCHT.xlsx")
-data_kontroly.to_pickle("Kontroly_VSCHT.pkl")
+
+# Načtení dat - načítat excel trvá dlouho, proto jej serializujeme jako pickle
+# data_kontroly = pd.read_excel("Kontroly_VSCHT.xlsx")
+# data_kontroly.to_pickle("Kontroly_VSCHT.pkl")
+data_kontroly = pd.read_pickle("Kontroly_VSCHT.pkl")
+# print(tabulate(data_kontroly.head(10), headers="keys"))
+
+# Data info - u několika sloupců se vyskytují prázdné buňky
+# data_kontroly.info()
+
+# Data describe
+print(tabulate(data_kontroly.describe(), headers="keys"))
 
