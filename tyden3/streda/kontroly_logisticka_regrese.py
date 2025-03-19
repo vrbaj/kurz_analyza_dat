79bd5ecc22551dd9a81324933572d4f6b6cd0533
stein <steinbach.jakub@gmail.com>
2025-03-19 08:20:18 +0100

    Středeční lekce - hledání trendů v datech o kontrolách

---
+++ tyden3/streda/kontroly_logisticka_regrese.py
@@ -0,0 +1,9 @@
+# Importy
+import pandas as pd
+from tabulate import tabulate
+from matplotlib import pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, balanced_accuracy_score
+
