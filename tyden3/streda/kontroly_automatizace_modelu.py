85cf3deac49312a4938bde064ddb2f53c82757e9
stein <steinbach.jakub@gmail.com>
2025-03-19 13:25:32 +0100

    Středeční lekce - automatizace trénování modelů

--- tyden3/streda/kontroly_automatizace_modelu.py
+++ tyden3/streda/kontroly_automatizace_modelu.py
@@ -10,3 +10,24 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, balanced_accuracy_score
 
+# Načtení dat
+data = pd.read_pickle("../utery/kontroly_ocistene.pkl")
+# Zapomenutá oprava druhu vozidla
+data["Druh_vozidla"] = data["Druh_vozidla"].fillna("nezjištěno")
+
+# Zákódování porušení
+data["Poruseni"] = data["Poruseni"].map({"ANO": 1, "NE": 0})
+
+# Definice parametrů jednotlivých modelů pro grid search
+parametry = {
+    "Logistická regrese": {"C": [0.001, 0.01, 0.1, 1, 10, 100, 1000]},
+    "Rozhodovací strom": {"max_depth": [5, 10, 20, None],
+                          "min_samples_split": [2, 5, 10, 20],
+                          "max_features": ["sqrt", "log2", None]},
+    "Náhodný les": {"max_depth": [5, 10, 20, None],
+                    "min_samples_split": [2, 5, 10, 20],
+                    "max_features": ["sqrt", "log2", None],
+                    "n_estimators": [5, 11, 21],
+                    "max_samples": [0.4, 0.6, 0.8, 1]
+                        },
+}
