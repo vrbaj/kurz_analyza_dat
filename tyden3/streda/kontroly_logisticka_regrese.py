83c844850aa6f95adbb2b84f1bd7cc28208bd577
stein <steinbach.jakub@gmail.com>
2025-03-19 08:24:10 +0100

    Středeční lekce - hledání trendů v datech o kontrolách

--- tyden3/streda/kontroly_logisticka_regrese.py
+++ tyden3/streda/kontroly_logisticka_regrese.py
@@ -7,3 +7,7 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, balanced_accuracy_score
 
+# Načtení dat
+# Dvě tečky pro přístup do složky o úroveň výše
+data = pd.read_pickle("../utery/kontroly_ocistene.pkl")
+print(tabulate(data.head(), headers="keys"))
