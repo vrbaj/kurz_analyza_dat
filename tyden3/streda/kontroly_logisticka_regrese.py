bf2f96a6aa47c978fe4870fafba99e334643d94d
stein <steinbach.jakub@gmail.com>
2025-03-19 08:41:13 +0100

    Středeční lekce - hledání trendů v datech o kontrolách

--- tyden3/streda/kontroly_logisticka_regrese.py
+++ tyden3/streda/kontroly_logisticka_regrese.py
@@ -10,6 +10,21 @@ from sklearn.metrics import classification_report, balanced_accuracy_score
 # Načtení dat
 # Dvě tečky pro přístup do složky o úroveň výše
 data = pd.read_pickle("../utery/kontroly_ocistene.pkl")
+data = data[['Celni_Urad', 'Poruseni', 'Kontrolni_Cinnost', 'Misto_kontoly',
+       'Cas_Zjisteni', 'Druh_vozidla', 'Stat', 'Pohlavi_porusitele', 'Utvar',
+       'datum_mesic', 'datum_den_tydne']]
+print(data.columns)
+
 # Problém s druhem vozidla - chybějící hodnoty
 data["Druh_vozidla"] = data["Druh_vozidla"].fillna("nezjištěno")
-print(data[pd.isna(data["Druh_vozidla"])].shape)
+
+# Volba mobilního dohledu
+data = data[data["Utvar"] == "Mobilní dohled"]
+
+# Zakódování sloupce Poruseni
+data["Poruseni"] = data["Poruseni"].map({"ANO": 1, "NE": 0})
+
+# Tabulka příznaků
+mazane_sloupce = ["Poruseni", "Kontrolni_Cinnost", "Utvar",
+                  "Pohlavi_porusitele", "Stat"]
+priznaky = data.drop(columns=mazane_sloupce)
