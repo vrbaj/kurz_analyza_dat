0ff3bfeae36d165cbb22a216b9e1e655f1cddfc3
stein <steinbach.jakub@gmail.com>
2025-03-19 10:21:14 +0100

    Středeční lekce - hledání trendů v datech o kontrolách

---
+++ tyden3/streda/kontroly_logisticka_regrese_zobecnene.py
@@ -0,0 +1,66 @@
+# Importy
+import pandas as pd
+from tabulate import tabulate
+from matplotlib import pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, balanced_accuracy_score
+
+
+# Nastavení seedu pro sjednocené generování náhodných hodnot
+np.random.seed(42)
+# Načtení dat
+# Dvě tečky pro přístup do složky o úroveň výše
+data = pd.read_pickle("../utery/kontroly_ocistene.pkl")
+data = data[['Celni_Urad', 'Poruseni', 'Kontrolni_Cinnost', 'Misto_kontoly',
+       'Cas_Zjisteni', 'Druh_vozidla', 'Stat', 'Pohlavi_porusitele', 'Utvar',
+       'datum_mesic', 'datum_den_tydne']]
+
+# Problém s druhem vozidla - chybějící hodnoty
+data["Druh_vozidla"] = data["Druh_vozidla"].fillna("nezjištěno")
+
+# Volba mobilního dohledu
+data = data[data["Utvar"] == "Mobilní dohled"]
+
+# Odfiltrování vybraných celních úřadů (Liberec, Moravskoslezský kraj)
+# print(data.shape)
+# data = data[(data["Celni_Urad"] != "CÚ pro Liberecký kraj") &
+#             (data["Celni_Urad"] != "CÚ pro Moravskoslezský kraj")]
+# print(data.shape)
+
+# Zakódování sloupce Poruseni
+data["Poruseni"] = data["Poruseni"].map({"ANO": 1, "NE": 0})
+
+# Tabulka příznaků
+mazane_sloupce = ["Poruseni", "Kontrolni_Cinnost", "Utvar",
+                  "Pohlavi_porusitele", "Stat", "Celni_Urad",
+                  "Misto_kontoly", "Druh_vozidla"]
+priznaky = data.drop(columns=mazane_sloupce)
+
+# one-hot encoding pro Pohlaví
+kodovane_pohlavi = pd.get_dummies(data[["Pohlavi_porusitele"]]).astype(int)
+
+# Sloučení tabulek příznaky a kódované pohlaví
+priznaky = pd.concat([priznaky, kodovane_pohlavi], axis=1)
+
+# Frekvenční kódování (frequency encoding) zbývajících sloupců
+kodovane_sloupce = ["Celni_Urad", "Misto_kontoly", "Druh_vozidla", "Stat"]
+# Iterace skrz kategorické sloupce
+for sloupec in kodovane_sloupce:
+    # Vytvoření relativních četností pro jednotlivé sloupce a uložení do slovníku
+    frekvencni_kodovani = data[sloupec].value_counts(normalize=True).to_dict()
+    # Namapování četností na jednotlivé hodnoty v daném sloupci a uložení jako
+    # nového sloucpce do tabulky s příznaky
+    priznaky[sloupec] = data[sloupec].map(frekvencni_kodovani)
+
+# Definice výstupu
+vystup = data["Poruseni"]
+
+# Inicializace modelu logistické regrese
+model = LogisticRegression(max_iter=1000, class_weight="balanced")
+parametry = {"C": [0.001, 0.01, 0.1, 1, 10, 100, 1000]}
+
+# Trénování modelu a vyhodnocení přesnosti klasifikace
+# Filtrace příznaků a výstupu podle konkrétní kontrolní činností
+cinnost = 'VV - tabák a tabákové výrobky'
