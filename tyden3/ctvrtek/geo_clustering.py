574422e16f9dbe8c596a8e2ca7b3cf69a75a8ed5
jirsat <45206873+jirsat@users.noreply.github.com>
2025-03-20 15:02:32 +0100

    Add data normalization for "Mnoz_v_Porušení" in geo_clustering.py using min-max scaling and logarithmic transformation.

--- tyden3/ctvrtek/geo_clustering.py
+++ tyden3/ctvrtek/geo_clustering.py
@@ -6,3 +6,20 @@ import matplotlib
 from matplotlib import pyplot as plt
 from sklearn.cluster import KMeans, DBSCAN, SpectralClustering
 from sklearn.preprocessing import minmax_scale
+
+data = pd.read_csv("ciste_opl.csv")
+
+
+for jednotka in data["Merna_jednotka"].unique():
+  for opl in data[data["Merna_jednotka"]==jednotka][
+                  "Druh_OPL"].unique():
+    aktualni_vysek = data.loc[
+      (data["Druh_OPL"]==opl) &
+      (data["Merna_jednotka"]==jednotka), 
+      "Mnoz_v_Porušení"]
+    aktualni_vysek = \
+      minmax_scale(
+        aktualni_vysek
+          .apply(lambda x: math.log(x-aktualni_vysek.min()+1))
+      )
+print(data["Mnoz_v_Porušení"].describe())
