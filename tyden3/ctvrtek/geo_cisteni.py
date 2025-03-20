968e4f7d9dc1aeec50af2b7c29d15dc48db201f4
jirsat <45206873+jirsat@users.noreply.github.com>
2025-03-20 12:56:50 +0100

    // Content: tyden3/ctvrtek/geo_cisteni.py

--- tyden3/ctvrtek/geo_cisteni.py
+++ tyden3/ctvrtek/geo_cisteni.py
@@ -3,3 +3,12 @@ import geopandas
 import seaborn as sns
 from matplotlib import pyplot as plt
 import matplotlib
+
+df = pandas.read_excel("OPL_VSCHT.xlsx", decimal=",")
+print(df.columns)
+
+for col in df.columns:
+    if col in ["Cas_Zjisteni"]:
+        continue
+    print("#"*50)
+    print(col, ": ", df[col].unique())
