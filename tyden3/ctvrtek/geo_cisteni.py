508c28d25a9c56fdc7906f5a37919e30484d80d7
jirsat <45206873+jirsat@users.noreply.github.com>
2025-03-20 13:12:45 +0100

    Update geo_cisteni.py to include additional columns in the filter and drop unnecessary columns

--- tyden3/ctvrtek/geo_cisteni.py
+++ tyden3/ctvrtek/geo_cisteni.py
@@ -8,7 +8,12 @@ df = pandas.read_excel("OPL_VSCHT.xlsx", decimal=",")
 print(df.columns)
 
 for col in df.columns:
-    if col in ["Cas_Zjisteni"]:
+    if col in ["Cas_Zjisteni","OsaX", "OsaY","OsaX_txt","OsaY_txt"]:
         continue
     print("#"*50)
     print(col, ": ", df[col].unique())
+
+df = df.drop(columns=["Kraj_zjisteni_poruseni","Okres_zjisteni_poruseni",
+                      "Bydliste_Porusitele","Narodnost_Porusitele","Typ_Vozidla",
+                      "Rozkaz_ID","Popis_Mista_Zjisteni","CisloTydne",
+                      "Km","Mesto","Kontrolni_Cinnost", "Duplicita","Utvar"])
