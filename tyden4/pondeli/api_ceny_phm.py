700d2800ad454eea1bc481ec727c5270713079bc
xr72 <Jan.Vrba@vscht.cz>
2025-03-24 16:09:58 +0100

    api_ceny_phm.py přidáno

--- tyden4/pondeli/api_ceny_phm.py
+++ tyden4/pondeli/api_ceny_phm.py
@@ -59,3 +59,31 @@ print(data_phm.shape)
 # Uložení indexů řádků (týdny)
 indexy_radky = list(json_phm["dimension"]["CASTPHM"]["category"]["index"].keys())
 
+# Indexy sloupců
+index_typ_ukazatele = list(json_phm["dimension"]["IndicatorType"]["category"]["label"].values())
+index_typ_paliva = list(json_phm["dimension"]["CENPHM"]["category"]["label"].values())
+print("---------- Indexy sloupců ----------")
+print(index_typ_ukazatele)
+print(index_typ_paliva)
+print(f"Počet řádků: {len(indexy_radky)}")
+
+# Zmnožení sloupců, aby z nich bylo možné vytvořit indexy v DataFrame
+# funkce repeat vytvoří pole, kde se za sebou několikrát opakuje každá z vložených hodnot
+typ_uka = np.repeat(index_typ_ukazatele, len(index_typ_paliva))
+# funkce tile zopakuje pole hodnot a spojí je dohromady
+typ_pal = np.tile(index_typ_paliva, len(index_typ_ukazatele))
+
+# MultiIndex
+multiindex = pd.MultiIndex.from_arrays([typ_uka, typ_pal])
+
+# Pandas DataFrame
+df_phm = pd.DataFrame(data_phm, columns=multiindex, index=indexy_radky)
+
+# Přetvoření indexu (číslo týdne) na časovou známku
+df_phm.index = "1-" + df_phm.index
+# to_datetime vezme textovou hodnotu a udělá z ní datum
+# v argumentu format určuji, kde se objevuje informace o datu v textovém řetězci
+# %w - reprezentuje číslo dne v týdnu, %Y reprezentuje rok, %U reprezentuje číslo týdne v roce
+df_phm.index = pd.to_datetime(df_phm.index, format="%w-%Y-W%U")
+
+print(tabulate(df_phm.head(), headers="keys"))
