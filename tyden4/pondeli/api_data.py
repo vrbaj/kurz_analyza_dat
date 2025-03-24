ca1c9d1bfd50bce7803bbeecb4c5ea870493d7d3
xr72 <Jan.Vrba@vscht.cz>
2025-03-24 13:20:40 +0100

    api_data.py hledání "index spotřebitelských cen"

--- tyden4/pondeli/api_data.py
+++ tyden4/pondeli/api_data.py
@@ -3,12 +3,29 @@ import pandas as pd
 from io import BytesIO
 import json
 
+# url dotazu ze kterého získáme json
 url_katalog = "https://data.csu.gov.cz/api/katalog/v1/vybery"
 
-r = requests.get(url_katalog)
-print(f"Status odpovědi: {r.status_code}")
-if r.status_code == 200:
-    with open("katalog_odpoved.json", "w") as f:
-        json.dump(r.json(), f)
-else:
-    print("Přenos selhal")
+# zavolání API a přečtení odpovědi
+# r = requests.get(url_katalog)
+# print(f"Status odpovědi: {r.status_code}")
+# if r.status_code == 200:
+#     # uložení odpovědi jako json
+#     with open("katalog_odpoved.json", "w") as f:
+#         json.dump(r.json(), f)
+# else:
+#     print("Přenos selhal")
+with open("katalog_odpoved.json") as f:
+    json_katalog = json.load(f)
+print(len(json_katalog))
+# print(json_katalog)
+print(json_katalog[0])
+print("-------------Klíčová slova vráceného slovníku--------------")
+print(json_katalog[0].keys())
+print(json_katalog[0]["vyber"])
+print("--------------------------------------------")
+print(json_katalog[0]["sada"])
+for datova_sada in json_katalog:
+    if "spotřebitelských cen" in datova_sada["vyber"]["nazev"]:
+        print(f"{datova_sada['vyber']['nazev']} -"
+              f" kód sady: {datova_sada['vyber']['kod']}")
