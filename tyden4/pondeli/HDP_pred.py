5f503c9575634660813a8ba1c933e5f687ff1b61
jirsat <45206873+jirsat@users.noreply.github.com>
2025-03-25 08:34:18 +0100

    stazeni dat o inflaci

--- tyden4/pondeli/HDP_pred.py
+++ tyden4/pondeli/HDP_pred.py
@@ -8,3 +8,8 @@ from tqdm import tqdm
 
 from matplotlib import pyplot as plt
 import seaborn as sns
+
+# Stažení dat z ČSÚ o inflaci
+base_url = "https://data.csu.gov.cz/api/dotaz/v1/data/vybery/"
+kod_sady = "CEN0101CT02"
+r = requests.get(base_url+kod_sady+"?format=JSON_STAT")
