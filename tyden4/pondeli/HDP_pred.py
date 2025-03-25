a4e24cd2337d8dd11d71861c4172b012358621d9
jirsat <45206873+jirsat@users.noreply.github.com>
2025-03-25 09:20:57 +0100

    nacteni dat reposazba

--- tyden4/pondeli/HDP_pred.py
+++ tyden4/pondeli/HDP_pred.py
@@ -38,4 +38,17 @@ df_hdp = pd.read_csv("hdp.csv")
 df_hdp = df_hdp.drop(["DATAFLOW","LAST UPDATE","freq","unit",
                       "s_adj","na_item","CONF_STATUS","OBS_FLAG"],axis=1)
 df_hdp = df_hdp.pivot(index="geo", columns="TIME_PERIOD", values="OBS_VALUE")
-print(df_hdp)
+#print(df_hdp)
+
+# Poptávka na trhu práce (eurostat) - https://ec.europa.eu/eurostat/databrowser/bookmark/6a758b9d-2ca4-4993-8b83-d21fdab92b4e?lang=en
+df_prace = pd.read_csv("labor_demand.csv")
+df_prace = df_prace.pivot(index="geo", columns="TIME_PERIOD", values="OBS_VALUE")
+#print(df_prace)
+
+# Data pro reposazbu
+df_sazba = pd.read_csv("reposazba.csv",delimiter=";",decimal=",")
+df_sazba.index = pd.to_datetime(df_sazba["Období"])
+df_sazba = df_sazba.resample("QS-DEC").first()
+df_sazba = df_sazba.drop(["Období","Unnamed: 2"] ,axis=1)
+df_sazba.columns = ["sazba"]
+
