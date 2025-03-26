5784ff1745bbf821510042e52224cd99819ea5d6
xr72 <Jan.Vrba@vscht.cz>
2025-03-26 09:09:14 +0100

    ar_detekce_mikro.py přidáno

---
+++ tyden4/streda/ar_detekce_mikro.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+import pandas as pd
+from statsmodels.tsa.arima.model import ARIMA
+
+#tabulka s výsledky predikcí pro jednotlivé firmy
+vysledky_predikci = pd.DataFrame(columns=["soubor", "chyba_predikce",
+                                          "skutecna_hodnota", "predikovana_hodnota",
+                                          "relativni_chyba"])
+print(vysledky_predikci)
+print(vysledky_predikci.columns)
+
+for soubor in Path(".", "mikro").iterdir():
+    print(f"Zpracovávám závěrku {soubor.name}")
+    zaverka = pd.read_excel(soubor)
+    #print(zaverka.head())
+    #print(zaverka.columns)
+    #print(zaverka["Nazev Polozky"])
+    sloupce_smazat = [sloupec for sloupec in zaverka.columns if sloupec.startswith("Unnamed")]
+    #print(sloupce_smazat)
+    zaverka.drop(sloupce_smazat, axis=1, inplace=True)
+    # print(zaverka.head())
+    # print(zaverka["Nazev Polozky"])
+    data_pro_ar = zaverka.loc[zaverka["Nazev Polozky"] == "HVzaUcetniObdobi"].copy()
+    # print(data_pro_ar)
+    data_pro_ar.drop(["Nazev Polozky"], axis=1, inplace=True)
+    # print("Data pro AR po smazání nenumerických hodnot")
+    # print(data_pro_ar)
+    X_ar = data_pro_ar.to_numpy()
+    # print(X_ar)
+    AR_trenovaci_data = X_ar[0, 0:-1]
+    # print("Trénovací data pro AR model")
+    # print(AR_trenovaci_data)
+    AR_testovaci_data = X_ar[0, -1]
+    # print("Testovací data pro AR model")
+    # print(AR_testovaci_data)
+    # AR model
+    ar_model = ARIMA(AR_trenovaci_data, order=(2, 0, 0))
+    natrenovany_model = ar_model.fit()
+    predikce = natrenovany_model.forecast(steps=1)
+    chyba_predikce = abs(AR_testovaci_data - predikce)
+    print(f"absolutní chyba predikce: {chyba_predikce} - "
+          f"očekávaná hodnota: {AR_testovaci_data} - "
+          f"predikce: {predikce}")
+
+
