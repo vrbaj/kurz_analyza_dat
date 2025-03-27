a7df452a5d79d2d99e2d8f27894d3ce432caf890
jirsat <45206873+jirsat@users.noreply.github.com>
2025-03-27 15:14:19 +0100

    nacitani dat

--- tyden4/ctvrtek/univerzal_mo.py
+++ tyden4/ctvrtek/univerzal_mo.py
@@ -21,3 +21,28 @@ from warnings import filterwarnings
 
 # Filtrování upozornění
 filterwarnings("ignore", category=EstimationWarning)
+
+def nacteni_dat(nazev_souboru="data_mo.xlsx"):
+  # nactu soubor
+  soubor = pd.ExcelFile(nazev_souboru)
+  # overeni souboru
+  print(soubor.sheet_names)
+  listy_ok = all([nazev_list in ["exog","endog"] for nazev_list
+                  in soubor.sheet_names])
+  assert listy_ok, "Špatný formát souboru"
+  # nacteni dat
+  endog = pd.read_excel(soubor, sheet_name="endog", index_col=0)
+  exog = pd.read_excel(soubor, sheet_name="exog", index_col=0)
+  endog.index = pd.to_datetime(endog.index)
+  exog.index = pd.to_datetime(exog.index)
+
+  # overim zda se indexy prekryvaji
+  spolecne_indexy = endog.index.intersection(exog.index)
+  assert len(spolecne_indexy) > 0, "Data se nepřekrývají"
+
+  # co chci predikovat 
+  budoucnost = exog.index.difference(endog.index)
+  budoucnost = budoucnost[budoucnost>endog.index[-1]]
+  assert len(budoucnost) > 0, "Není definované období pro predikci"
+
+nacteni_dat()
