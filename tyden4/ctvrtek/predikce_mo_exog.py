33665cd174d6badd094df390a481370f4ed32957
jirsat <45206873+jirsat@users.noreply.github.com>
2025-03-27 08:37:36 +0100

    stazeni dat

--- tyden4/ctvrtek/predikce_mo_exog.py
+++ tyden4/ctvrtek/predikce_mo_exog.py
@@ -27,3 +27,17 @@ soubor_inflace = Path("inflace_pro_mo.csv")
 if not soubor_inflace.exists():
   base_url = "https://data.csu.gov.cz/api/dotaz/v1/data/vybery/"
   kod_sady = "CEN0101CT02"
+
+  r = requests.get(base_url+kod_sady+"?format=JSON_STAT")
+
+  slovnik_inflace = r.json()
+  nazvy_sloupcu = list(
+    slovnik_inflace["dimension"]["CasM"]["category"]["index"].keys()
+  )
+
+  df_inflace = pd.DataFrame(
+    np.array(slovnik_inflace["value"]).reshape(-1,len(nazvy_sloupcu)),
+    columns=nazvy_sloupcu,
+    index=(slovnik_inflace["dimension"]["CZCOICOP"]
+                          ["category"]["label"].values())
+  )
