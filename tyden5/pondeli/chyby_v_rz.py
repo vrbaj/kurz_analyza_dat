a4751b99a6061026199d65bf6c3c4d1547267de3
vrbaj <vrbik.vrbik@gmail.com>
2026-04-20 16:10:44 +0200

    hledani chyb v RZ

--- tyden5/pondeli/chyby_v_rz.py
+++ tyden5/pondeli/chyby_v_rz.py
@@ -8,6 +8,17 @@ data = pd.read_csv("umele_rz.csv")
 # Prevedeni vsech pismen na velka
 data["RZ"] = data["RZ"].apply(lambda x: x.upper())
 
+# Odstraneni mezer / dalsich znamych nechtenych oddelovacu
+data["RZ"] = data["RZ"].apply(lambda x: x.replace(" ", "").replace("-", ""))
 
+# Validace kodu RZ
+data["je_validni"] = True
 
-print(tabulate(data.head(), headers="keys", tablefmt="psql"))
+# Kontrola poctu znaku
+filtr = (data["RZ"].str.len() < 5) | (data["RZ"].str.len() > 8)
+data.loc[filtr, "je_validni"] = False
+
+# Kontrola povolenych znaku
+povolene_znaky = set("ABCDEFHIJKLMNPRTUVXYZ0123456789")
+
+print(tabulate(data, headers="keys", tablefmt="psql"))
