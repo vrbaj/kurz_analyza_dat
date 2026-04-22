1ffd62de83f8d4d69c8302ee268422f30f041755
vrbaj <vrbik.vrbik@gmail.com>
2026-04-22 08:28:19 +0200

    streda - geolokace uvod

--- tyden5/streda/geo_outliery.py
+++ tyden5/streda/geo_outliery.py
@@ -1,13 +1,28 @@
 
 import pyodbc
+import pandas as pd
 
-CONN_STR = (
-    "DRIVER={ODBC Driver 18 for SQL Server};"
-    "SERVER=(localdb)\MSSQLLocalDB;"
-    "DATABASE=MD2_VSCHT;"
-    "Trusted_Connection=yes;"
-    "TrustServerCertificate=no;"
-)
+DB = False
 
-conn = pyodbc.connect(CONN_STR)
-print(conn)
+if DB:
+    # definice připojení k DB
+    CONN_STR = (
+        "DRIVER={ODBC Driver 18 for SQL Server};"
+        "SERVER=(localdb)\MSSQLLocalDB;"
+        "DATABASE=MD2_VSCHT;"
+        "Trusted_Connection=yes;"
+        "TrustServerCertificate=no;"
+    )
+    # vytvoření připojení
+    conn = pyodbc.connect(CONN_STR)
+    # požadovaná SQL query
+    query = """
+    SELECT d.axisx, d.axisy, a.AuditAction, d.isCrimact
+    FROM inetuser.MDx_Disorder d
+    LEFT JOIN inetuser.MDx_AuditAction a ON d.cAuditAction = a.cAuditAction"""
+    # načtení dat z db
+    lokace = pd.read_sql(query, conn)
+    # uložení do CSV
+    lokace.to_csv("lokace.csv")
+else:
+    lokace = pd.read_csv("lokace.csv")
