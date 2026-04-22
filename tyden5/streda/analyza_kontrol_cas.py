18a5d71517ee197e4e764449c85500e9b1f4184d
vrbaj <vrbik.vrbik@gmail.com>
2026-04-22 14:29:13 +0200

    streda - analyza kontrol v case vykop

--- tyden5/streda/analyza_kontrol_cas.py
+++ tyden5/streda/analyza_kontrol_cas.py
@@ -33,5 +33,29 @@ conn.close()
 
 # kontrola výsledku query
 print(df.head())
+# zjištění celkového počtu záznamů
 print(df.shape)
+# informace o sloupcích
 print(df.columns)
+
+# převedeme sloupec LocDate na datetime objekt
+df["LocDate"] = pd.to_datetime(df["LocDate"])
+# převedení datumu na den v týdnu
+df["den_v_tydnu"] = df["LocDate"].dt.dayofweek
+
+# pondělí = 0, neděle = 6
+print(df["den_v_tydnu"])
+
+# kontrola LocTime
+print(df["LocTime"])
+# extrakce hodiny ze sloupce LocTime
+df["hodina"] = df["LocTime"].str.replace(".", ":", regex=False).str.split(":")[0]
+
+# převod textu na číslo, v případě že to nejde tak NaN
+df["hodina"] = pd.to_numeric(df["hodina"], errors="coerce")
+# kontrola velikost
+print(df.shape)
+# odstranění NaN hodnot
+df.dropna(subset=["hodina"], inplace=True)
+# kontrola velikosti
+print(df.shape)
