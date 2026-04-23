b28dd0c193666f69e6b078eeea66bc4a95f19298
vrbaj <vrbik.vrbik@gmail.com>
2026-04-23 08:22:05 +0200

    streda - analýza časová finish

--- tyden5/ctvrtek/xgboost_brent.py
+++ tyden5/ctvrtek/xgboost_brent.py
@@ -1,13 +1,14 @@
 # definovanání použitých knihoven/modulů
 
- import warnings
- from dataclasses import dataclass
- from typing import Tuple, List, Dict
+import warnings
+from dataclasses import dataclass
+from typing import Tuple, List, Dict
 
- import numpy as np
- import pandas as pd
- import matplotlib.pyplot as plt
- from sklearn.metrics import mean_absolute_error, mean_squared_error
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 
- # potlačení varování pomocí knihovny warnings
- warnings.filterwarnings("ignore")
+# potlačení varování pomocí knihovny warnings
+warnings.filterwarnings("ignore")
+
