73a88e30b837bae180c3bc3f67dd08527a2b47f8
vrbaj <vrbik.vrbik@gmail.com>
2026-04-23 08:25:15 +0200

    streda - analýza časová finish

--- tyden5/ctvrtek/xgboost_brent.py
+++ tyden5/ctvrtek/xgboost_brent.py
@@ -12,3 +12,14 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error
 # potlačení varování pomocí knihovny warnings
 warnings.filterwarnings("ignore")
 
+# nahrání dat z webu
+SERIES: Dict[str,str] = {
+    # denní cena ropy brent
+    "brent": "https://fred.stlouisfed.org/graph/fredgraph.csv?id=DCOILBRENTEU",
+    # index dolaru
+    "usd_index": "https://fred.stlouisfed.org/graph/fredgraph.csv?id=DTWEXBGS",
+    # index volatility
+    "vix": "https://fred.stlouisfed.org/graph/fredgraph.csv?id=VIXCLS",
+    # index průmyslové produkce USA
+    "indpro": "https://fred.stlouisfed.org/graph/fredgraph.csv?id=INDPRO",
+}
