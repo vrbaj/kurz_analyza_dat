732e5392692db2b8720fe751f52eae5d76a9eb38
jirsat <45206873+jirsat@users.noreply.github.com>
2026-04-24 08:50:46 +0200

    cpa - prompt

--- tyden5/patek/analayza_llm.py
+++ tyden5/patek/analayza_llm.py
@@ -8,8 +8,8 @@ from tabulate import tabulate
 # Nacteni dat
 data = pd.read_csv(Path("Kontroly_MD_poruseni-zbozi.csv"))
 
-print(data.head())
-print(data.columns)
+#print(data.head())
+#print(data.columns)
 
 k_analyze = [
     "TypMista",
@@ -20,4 +20,20 @@ k_analyze = [
     "Poznamka",
 ]
 
-print(tabulate(data[k_analyze].head(), headers="keys", tablefmt="psql"))
+#print(tabulate(data[k_analyze].head(), headers="keys", tablefmt="psql"))
+
+data = data[k_analyze]
+
+system_prompt = """Jsi datový analytik celní správy.
+Tvým úkolem je kontrolovat hlášení hlídek a objevit případné špatné hlášení.
+Kontroluj pouze dodané informace, nesnaž se vymýšlet něco navíc. 
+Najdi pouze jednoznačně špatné informace.
+"""
+
+# vytvoříme funkci k analýze jednoho řádku a převedení na user prompt
+def vytvor_uzivatelsky_prompt(row) -> str:
+  if pd.isna(row["Poznamka"]):
+    row["Poznamka"] = "Bez slovního doplnění"
+  if pd.isna(row["DruhVozidla"]):
+    row["DruhVozidla"] = "Žádné"
+  row["Komodita"] = row["Komodita"].replace("VV - ", "")
